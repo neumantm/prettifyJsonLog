1193b01c71c3e5a237342b474e2e7793ca40aaa7
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2022-07-29 17:20:03 +0200

    Add long description

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ from setuptools import setup, find_packages
 setup(name='prettifyJsonLog',
       version='1.0',
       description='A small python programm to make json log formats human readable',
+      long_description="""
+      A small python programm to make json log formats human readable
+
+      It reads log lines from stdin.
+      Each line must be one log entry formatted as a JSON object.
+      """
       url="https://github.com/neumantm/prettifyJsonLog",
       license="MIT",
       author='Tim Neumann',
