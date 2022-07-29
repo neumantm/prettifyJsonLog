6c9d47645940a36b5a8c03929acd25c1921025ad
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2022-07-29 17:44:00 +0200

    feat(setup.py): Make long_description be the README

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
+
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
 
 setup(name='prettifyJsonLog',
       version='1.0',
       description='A small python programm to make json log formats human readable',
-      long_description="""
-      A small python programm to make json log formats human readable
-
-      It reads log lines from stdin.
-      Each line must be one log entry formatted as a JSON object.
-      """,
+      long_description=README,
       url="https://github.com/neumantm/prettifyJsonLog",
       license="MIT",
       author='Tim Neumann',
       author_email='neuamntm@fius.informatik.uni-stuttgart.de',
 
+      include_package_data=True,
       packages=find_packages(),
       entry_points = {
           'console_scripts': ['prettifyJsonLog=prettifyJsonLog.prettifyJsonLog:main'],
